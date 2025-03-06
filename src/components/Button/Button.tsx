59683fc8c9e162557bc020fa407ec190c996c266
s.sergeev <s.sergeev@rsvn.ru>
2025-03-06 03:23:22 +0300

    add default type to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,9 +4,9 @@ import {ButtonProps} from "./types";
 import {Spinner} from "../Spinner";
 import './style.css'
 
-const Button: FC<ButtonProps> = ({children, loading, variant = 'primary', ...props}) => {
+const Button: FC<ButtonProps> = ({children, loading, variant = 'primary', type = 'button', ...props}) => {
   return (
-    <button {...props} className={`button button--${variant}`}>
+    <button {...props} type={type} className={`button button--${variant}`}>
       {children}
       {loading && <Spinner size='sm'/>}
     </button>
